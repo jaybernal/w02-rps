2dc32e372597e2805851ccc9fc5e00174c69b3f7
Jay Bernal <jjbernal@gmail.com>
2016-03-24 23:15:14 -0500

    completed phase 0

--- lib/paper_rock_scissors.rb
+++ lib/paper_rock_scissors.rb
@@ -1,41 +1,44 @@
+require "pry"
+#phase 0
 # Make a prompt for player_1 & player_2 to make a choice
 #choices: paper, rock, scissors
 #paper > rock
 #rock > scissors
 #scissors > paper 
-
 #tie if: paper == paper || rock == rock || scissors == scissors
 
 
-puts "Lets play Paper, Rock, Scissors!!!"
+#phase 1
+# Ask for the players name
+# Display the results of the game.
+# Ask how many games the user wants to play. (odds numbers only)
+# Keep track of the games won by each user
 
-puts "Player one: Pick your game piece"
-choice1 = gets.chomp.downcase
+	puts "Lets play Paper, Rock, or Scissors!!!  Player one, what your name?"
+	player_1 = gets.chomp.capitalize
 
-puts "Player two: Pick your game piece"
-choice2 = gets.chomp.downcase
+	puts "Player two, what is your name?" 
+	player_2 = gets.chomp.capitalize
 
+	puts "#{player_1} & #{player_2} how many games do you want to play? Pick odd numbers"
+	number_of_games = gets.chomp
 
-if choice1 == "paper" && choice2 == "rock"
-	puts "Player one wins, #{choice1} covers #{choice2}"
-elsif choice1 = "paper" && choice2 == "scissors"
-	puts "Player two wins, #{choice1} cuts #{choice2}"
-elsif choice1 == "rock" && choice2 == "paper"
-	puts "Player two wins, #{choice2} covers #{choice1}"
-elsif choice1 = "rock" && choice2 == "scissors"
-	puts "Player one wins, #{choice1} cuts #{choice2}"
-elsif choice1 == "scissors" && choice2 == "rock"
-	puts "Player two wins, #{choice2} smashes #{choice1}"
-elsif choice1 = "scissors" && choice2 == "paper"
-	puts "Player one wins, #{choice1} cuts #{choice2}"
-else
-	puts "its a tie"
-end
+	puts "#{player_1}, Pick your weapon for this round"
+	choice_1 = gets.chomp.downcase
+	puts "#{player_2}, Pick your weapon for this round"
+	choice_2 = gets.chomp.downcase
 
+	if 	choice_1 		== "paper" && choice_2 == "rock" || 
+			choice_1 		== "rock" && choice_2 == "scissors" ||
+			choice_1		== "scissors" && choice_2 == "paper"
+		puts "#{player_1} wins"
 
-
-
-
-
+	elsif choice_1 == "paper" && choice_2 == "scissors" ||
+				choice_1 == "scissors" && choice_2 == "rock" ||
+				choice_1 == "rock" && choice_2 == "paper"
+		puts "#{player_2} wins"
+	else
+		puts "its a tie, try again"
+	end
 
 
